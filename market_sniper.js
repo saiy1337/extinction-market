e30b68ca1ead0a512caf74c0e8bc251bcd7b456e
Dunk#1337 <40726857+dunk1337@users.noreply.github.com>
2021-02-23 15:05:38 -0500

    Revert "Update code, thanks to @fweak"
    This reverts commit 0fc3895b5ea0ca82ed6ed14d9eed9599d7b099f7.

--- market_sniper.js
+++ market_sniper.js
@@ -1,29 +1,37 @@
 const fs = require('fs');
 const axios = require('axios')
 const config = JSON.parse(fs.readFileSync('config.json'));
+var priceArray = config.weapons;
+var doneOffers = [];
 
-let priceArray = config.weapons;
-let doneOffers = [];
+function formatNumber(n) {
+  return n.toString().replace(/(\d)(?=(\d{3})+(?!\d))/g, '$1,');
+}
 
-const formatNumber = (n) => n.toString().replace(/(\d)(?=(\d{3})+(?!\d))/g, '$1,');
+const Discord = require('discord.js');
+const bot = new Discord.Client();
 
-setInterval( async () => {
-	const item = priceArray[~~(Math.random() * priceArray.length)];
-	const name = item.sanitized_name;
+function checkMarketplace() {
+  var item = priceArray[~~(Math.random() * priceArray.length)];
+  const item_name = item.sanitized_name;
+  try {
+    axios.get(`https://api.gtaliferp.fr:8443/v1/extinction/marketplace/sell/${item.name}`)
+      .then(function (response) {
+        const data = response.data;
+        data.forEach((offer) => {
+          if (offer.price < item.price && !(doneOffers.find(v => (offer.id.toString() === v)))) {
+            axios.post(config.webhook_link, {content : `@here An item has been put on sale **${item_name}** at the price of **${formatNumber(offer.price)}$** by **${offer.seller}**!`})
+            doneOffers.push(offer.id.toString());
+            return;
+          }
+        })
+      });
+  }
+  catch (err) {
+    return console.log(err);
+  }
+}
 
-	try {
-		const request = await axios.get(`https://api.gtaliferp.fr:8443/v1/extinction/marketplace/sell/${item.name}`);
+setInterval(checkMarketplace, config.interval);
 
-		const data = request.data;
-
-		data.forEach((offer) => {
-			if (offer.price < item.price && !(doneOffers.find(v => (offer.id.toString() === v)))) {
-				axios.post(config.webhook_link, { content: `@here An item has been put on sale **${name}** at the price of **${formatNumber(offer.price)}$** by **${offer.seller}**!`});
-				doneOffers.push(offer.id);
-
-			}
-		});
-	} catch (err) {
-		return console.error(err);
-	}
-} , config.interval);
+bot.login(config.token);
