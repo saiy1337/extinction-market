0fc3895b5ea0ca82ed6ed14d9eed9599d7b099f7
Dunk#1337 <40726857+dunk1337@users.noreply.github.com>
2021-02-23 14:48:34 -0500

    Update code, thanks to @fweak

--- market_sniper.js
+++ market_sniper.js
@@ -1,37 +1,29 @@
 const fs = require('fs');
 const axios = require('axios')
 const config = JSON.parse(fs.readFileSync('config.json'));
-var priceArray = config.weapons;
-var doneOffers = [];
 
-function formatNumber(n) {
-  return n.toString().replace(/(\d)(?=(\d{3})+(?!\d))/g, '$1,');
-}
+let priceArray = config.weapons;
+let doneOffers = [];
 
-const Discord = require('discord.js');
-const bot = new Discord.Client();
+const formatNumber = (n) => n.toString().replace(/(\d)(?=(\d{3})+(?!\d))/g, '$1,');
 
-function checkMarketplace() {
-  var item = priceArray[~~(Math.random() * priceArray.length)];
-  const item_name = item.sanitized_name;
-  try {
-    axios.get(`https://api.gtaliferp.fr:8443/v1/extinction/marketplace/sell/${item.name}`)
-      .then(function (response) {
-        const data = response.data;
-        data.forEach((offer) => {
-          if (offer.price < item.price && !(doneOffers.find(v => (offer.id.toString() === v)))) {
-            axios.post(config.webhook_link, {content : `@here An item has been put on sale **${item_name}** at the price of **${formatNumber(offer.price)}$** by **${offer.seller}**!`})
-            doneOffers.push(offer.id.toString());
-            return;
-          }
-        })
-      });
-  }
-  catch (err) {
-    return console.log(err);
-  }
-}
+setInterval( async () => {
+	const item = priceArray[~~(Math.random() * priceArray.length)];
+	const name = item.sanitized_name;
 
-setInterval(checkMarketplace, config.interval);
+	try {
+		const request = await axios.get(`https://api.gtaliferp.fr:8443/v1/extinction/marketplace/sell/${item.name}`);
 
-bot.login(config.token);
+		const data = request.data;
+
+		data.forEach((offer) => {
+			if (offer.price < item.price && !(doneOffers.find(v => (offer.id.toString() === v)))) {
+				axios.post(config.webhook_link, { content: `@here An item has been put on sale **${name}** at the price of **${formatNumber(offer.price)}$** by **${offer.seller}**!`});
+				doneOffers.push(offer.id);
+
+			}
+		});
+	} catch (err) {
+		return console.error(err);
+	}
+} , config.interval);
